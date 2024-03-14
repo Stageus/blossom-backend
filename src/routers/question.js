eda64d1798a2ebc3d7ee6bb3860a08a672b4116a
Sohyunxxi <dlthgus15780@gmail.com>
2024-03-14 15:53:43 +0900

    add question makelog, update sql

--- src/routers/question.js
+++ src/routers/question.js
@@ -3,12 +3,9 @@ const isLogin = require('../middleware/isLogin');
 const queryConnect = require('../modules/queryConnect');
 const makeLog = require("../modules/makelog");
 const isBlank = require("../middleware/isBlank")
-const redis = require("redis").createClient();
 
 // 문답 전체 목록 불러오기 API
-router.get("/", isLogin, async (req, res, next) => {
-    const userId = req.user.id;  
-    const coupleIdx = req.user.coupleIdx;
+router.get("/question/all", isLogin, async (req, res, next) => {
     const result = {
         success: false,
         message: "",
@@ -18,16 +15,14 @@ router.get("/", isLogin, async (req, res, next) => {
     };
 
     try {
-        const query = { //couple_idx 추가해야할듯? -> 그 커플에게 할당된 질문을 확인하기 위해서
-            text: `
-                SELECT 
-                    *, 
-                FROM 
-                    question
-                WHERE
-                    couple_idx=$1
+        const query = { //커플 create_at에서 현재 날짜를 뺀 그 총 일수만큼 question 불러오기
+            text: ` SELECT q.*
+                    FROM question q
+                    JOIN couple c ON q.couple_idx = c.idx
+                    WHERE c.idx = $1
+                    AND q.create_at >= CURRENT_TIMESTAMP - INTERVAL '7 days'
+                    ORDER BY q.create_at DESC;
             `,
-            values: [coupleIdx],
         };
         
         const { rows } = await queryConnect(query);
@@ -37,6 +32,18 @@ router.get("/", isLogin, async (req, res, next) => {
         result.message = "질문 목록 불러오기 성공";    
         res.send(result);
 
+        const logData = {
+            ip: req.ip,
+            userId: id,
+            apiName: '/question/all',
+            restMethod: 'get',
+            inputData: {  },
+            outputData: result,
+            time: new Date(),
+        };
+    
+        makeLog(req, res, logData, next);
+
     } catch (error) {
         console.error('질문 목록 불러오기 오류: ', error);
         result.message = "질문 목록 불러오기 실패";
@@ -44,58 +51,18 @@ router.get("/", isLogin, async (req, res, next) => {
     }
 });
 
-// 문답 검색하기 => 명세서에 없는데...?
-router.get("/search", isLogin, async (req, res, next) => {
-    const userIdx = req.user.idx
-    const userId = req.user.id
-    const { date } = req.query // query 안쓴다했는데 get은 시간 검색시 써야하지않나?
-    const time = new Date() //timestamp
-    const result = {
-        "message": "",
-        "data": {
-            "searchedQuestion": null,
-        }
-    }
-    try { 
-       
-        const query = {
-            text: `
-                SELECT 
-                    *
-                FROM 
-                    question
-                WHERE 
-                    couple_idx = $1
-            `,
-            values: [`%${title}%`],
-        };
-        const {rowCount, rows}= await queryConnect(query);
-
-        if (rowCount == 0) {
-            result.message = "게시물 없음."
-        } else {
-            result.data.searchPost = rowCount
-            result.message = rows;
-        }
-       
-        return res.status(200).send(result);
-    } catch (error) {
-        next(error)
-    } finally {
-        await redis.disconnect()
-    }
-})
-
-
 // 특정 문답 불러오기 API
-router.get("/:questionIdx", isLogin, async (req, res, next) => {
+router.get("/question/:idx", isLogin, async (req, res, next) => {
     const questionIdx = req.params.questionIdx;
     const userIdx = req.user.idx;
     const coupleIdx = req.user.coupleIdx;
     const result = {
         success: false,
         message: "",
-        data: null,
+        data: {
+            myAnswer: null,
+            partnerAnswer:null
+        },
     };
     try {
 
@@ -114,77 +81,95 @@ router.get("/:questionIdx", isLogin, async (req, res, next) => {
 
         if (rows.length == 0) {
             return next({
-                message: '게시물 불러오기 실패',
+                message: '상대방 idx 불러오기 실패',
                 status: 500
             });
         } 
 
         partnerIdx = rows[0].partner_idx;
 
-        const query = { // 어려워.... 답변 2개 나올것. 그런데 그 답변에 해당하는 idx 가져와서 넣기?
-            text: ` SELECT 
-                        q.question, 
-                        a.content,
-                        a.account_idx AS answerer_idx
-                    FROM 
-                        question q
-                    JOIN 
-                        answer a ON q.idx = a.question_idx
-                    WHERE 
-                        q.idx = $1 AND 
-                        q.couple_idx = $2;`
+        const partnerQuery = {
+            text: ` SELECT * FROM answer
+                    WHERE account_idx = $1
+                    AND question_idx = $2;`
             ,
-            values: [questionIdx, coupleIdx],
+            values: [coupleIdx, questionIdx],
         };
 
-        const findResult = await queryConnect(query);
+        const findResult = await queryConnect(partnerQuery);
         const findRows = findResult.rows[0]
 
         if (findRows.length == 0) {
             return next({
-                message: '답변 불러오기 실패',
+                message: '상대 답변 불러오기 실패',
                 status: 500
             });
         } 
 
-        const question = findRows[0].question;
+        const myQuery = {
+            text: ` SELECT * FROM answer
+                    WHERE account_idx = $1
+                    AND question_idx = $2;`
+            ,
+            values: [userIdx, questionIdx],
+        };
 
-        //답변 분류하기 => 여기가 고민중... if문써서??
-        const myAnswer = findRows[0].content;
-        const partnerAnswer = findRows[0].content;
+        const myResult = await queryConnect(myQuery);
+        const myRows = myResult.rows[0]
+
+        if (myRows.length == 0) {
+            return next({
+                message: '내 답변 불러오기 실패',
+                status: 500
+            });
+        } 
+
+        const sqlMyAnswer = findRows[0].content;
+        const sqlPartnerAnswer = myRows[0].content;
+
+        result.data.myAnswer = sqlMyAnswer;
+        result.data.partnerAnswer = sqlPartnerAnswer;
 
         result.success = true;
-        result.data = post; 
         
         res.send(result);
+
+        const logData = {
+            ip: req.ip,
+            userId: id,
+            apiName: '/question/:idx',
+            restMethod: 'get',
+            inputData: {  },
+            outputData: result,
+            time: new Date(),
+        };
+    
+        makeLog(req, res, logData, next);
     } catch (error) {
-        console.error('게시물 가져오기 오류 발생: ', error.message);
+        console.error('답변 가져오기 오류 발생: ', error.message);
         result.message = error.message;
         return next(error);
     }
 });
 
-// 문답 답변 쓰기 API 
-router.post("/:questionIdx", isLogin, isBlank('content'), async (req, res, next) => {
+// 문답 답변 쓰기 API -> 수정하기
+router.post("question/:idx", isLogin, isBlank('content'), async (req, res, next) => {
     const userIdx = req.user.idx;
     const coupleIdx = req.user.coupleIdx;
     const questionIdx = req.params.questionIdx;
     const { content } = req.body;
-    const files = req.files;
     const result = {
         success: false,
         message: "",
         data: null
     };
-    console.log("파일들: ",files)
     try {
-        // 포스트 테이블에 게시물 등록
-        const postInsertQuery = {
+        const answerInsertQuery = {
             text: 'INSERT INTO answer (content, account_idx, couple_idx, question_idx) VALUES ($1, $2, $3, $4)',
             values: [content, userIdx, coupleIdx, questionIdx]
         };
 
-        const { rowCount } = await queryConnect(postInsertQuery);
+        const { rowCount } = await queryConnect(answerInsertQuery);
         if(rowCount==0){
             return next({
                 message: '답변 작성 실패',
@@ -193,9 +178,20 @@ router.post("/:questionIdx", isLogin, isBlank('content'), async (req, res, next)
         }
         result.success = true;
         result.message = "답변 등록 성공";
-        //result.data = {};
 
         res.send(result);
+
+        const logData = {
+            ip: req.ip,
+            userId: id,
+            apiName: '/question/:idx',
+            restMethod: 'post',
+            inputData: { content },
+            outputData: result,
+            time: new Date(),
+        };
+    
+        makeLog(req, res, logData, next);
 
     } catch (e) {
         result.message = e.message;
