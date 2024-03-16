3c82f4e1aba118704928f2fe2bbdfc060c39b8c2
hj-lab <heeju03180@naver.com>
2024-03-16 21:44:12 +0900

    Add error code, middleware to schedule.js

--- src/config/patterns.js
+++ src/config/patterns.js
@@ -5,6 +5,9 @@ const nicknameReq = /^[a-zA-Z가-힣]{2,50}$/
 const telReq = /^[0-9]{11}$/
 const dateReq = /^\d{4}(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])$/
 const imageReq = /\.(jpg|jpeg|png|gif)$/i;
-const commentReq = /^.{1,50}$/;
+const commentReq = /^.{1,50}$/; // 댓글 1~50자
+const timestampReq = /\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01]) (0[0-9]|1[0-9]|2[0-3]):(0[1-9]|[0-5][0-9]):(0[1-9]|[0-5][0-9])$/;
+// YYYY-MM-DD HH:mm:ss
+const scheduleReq = /^.{1,20}$/; // 일정 1~20자
 
-module.exports ={idReq,pwReq,nameReq,nicknameReq,imageReq,telReq,dateReq,commentReq};
+module.exports ={idReq,pwReq,nameReq,nicknameReq,imageReq,telReq,dateReq,commentReq,timestampReq,scheduleReq};
