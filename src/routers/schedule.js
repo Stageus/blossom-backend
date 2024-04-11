a34f44c3064417d297ff444f1eb167b837410fe9
hj-lab <heeju03180@naver.com>
2024-04-11 21:05:05 +0900

    delete isMycouple from schedule.js

--- src/routers/schedule.js
+++ src/routers/schedule.js
@@ -4,18 +4,18 @@ const checkPattern = require("../middleware/checkPattern");
 const isBlank = require("../middleware/isBlank");
 const { executeSQL } = require("../modules/sql");
 const isLogin = require("../middleware/isLogin");
-const { isMycouple } = require("../modules/isMycouple");
 
 const { idReq,pwReq,nameReq,nicknameReq,imageReq,telReq,dateReq,timestampReq,scheduleReq}= require("../config/patterns");
 
 const conn = require("../config/postgresql");
 
+// 공통 TODO : coupleIdx는 islogin 추가해서 req.user에서 받아오도록 하기
+
 // 1.get schedule/all 특정 월의 전체 일정 불러오기
-// TODO : coupleIdx는 islogin 추가해서 req.user에서 받아오도록 하기
 // TODO : req.body에서 date를 받을건지 year,month를 받을건지 결정하고 그에대한 정규식 체크 미들웨어 추가하기
-router.get("/all", async(req, res, next) => {
-    // const { coupleIdx } = req.user;
-    const { coupleIdx } = req.body; // test용
+router.get("/all", isLogin, async(req, res, next) => {
+    const { coupleIdx } = req.user;
+    // const { coupleIdx } = req.body; // test용
     const { year,month } = req.body; // 년, 월만 받으면됨 --> Year, Month 각각 받는게 나은지?
     
     const result = {
@@ -53,8 +53,6 @@ router.get("/all", async(req, res, next) => {
 })
 
 // 2.get schedule 특정 날짜의 일정 불러오기
-// TODO : coupleIdx는 islogin 추가해서 req.user에서 받아오도록 하기
-// date는 년,월,일 까지 => FE에서 년/월/일 따로 받아서 비교하는게 편할까
 // TODO : req.body에서 date를 받을건지 year,month를 받을건지 결정하고 그에대한 정규식 체크 미들웨어 추가하기
 router.get("/", async(req, res, next) => {
     // const { coupleIdx } = req.user;
@@ -95,7 +93,6 @@ router.get("/", async(req, res, next) => {
 })
 
 // 3.post schedule 일정 추가하기
-// TODO : coupleIdx, accountIdx는 islogin 미들웨어 넣어서 req.user에서 가져오도록 바꿔야함
 router.post("/", checkPattern(scheduleReq, "content"), checkPattern(timestampReq, "date"), async(req,res,next) => {
     // const { coupleIdx, accountIdx } = req.user; // isLogin에서 token해석해서 전달
     const { coupleIdx } = req.body; // test용
@@ -125,8 +122,6 @@ router.post("/", checkPattern(scheduleReq, "content"), checkPattern(timestampReq
 })
 
 // 4. put feed/:idx 특정 일정 수정하기
-// TODO : coupleIdx는 islogin 미들웨어 넣어서 req.user에서 가져오도록 바꿔야함
-// TODO : isMycouple 미들웨어 / 모듈 문제점 ==> 근데 이 다음에 test한 feed 에서는 또 잘됨 => why?
 router.put("/:idx", checkPattern(scheduleReq, "content"), checkPattern(timestampReq, "date"), async(req, res, next) => {
     // const { coupleIdx } = req.user;
     const { coupleIdx } = req.body; // test용
@@ -139,8 +134,6 @@ router.put("/:idx", checkPattern(scheduleReq, "content"), checkPattern(timestamp
     };
 
     try{
-        await isMycouple(coupleIdx, scheduleIdx, "schedule");
-
         const sql = `UPDATE schedule SET content = $1, date = $2 WHERE idx = $3 AND couple_idx = $4`
         const values = [content, date, scheduleIdx, coupleIdx]
         
@@ -157,8 +150,6 @@ router.put("/:idx", checkPattern(scheduleReq, "content"), checkPattern(timestamp
 })
 
 // 5.delete schedule/:idx 특정 일정 삭제하기
-// TODO : coupleIdx는 islogin 미들웨어 넣어서 req.user에서 가져오도록 바꿔야함
-// TODO : isMycouple 미들웨어 / 모듈 문제점
 router.delete("/:idx", async(req, res, next) => {
     // const { coupleIdx }  = req.user;
     const { coupleIdx } = req.body; // test용
@@ -170,8 +161,6 @@ router.delete("/:idx", async(req, res, next) => {
     };
 
     try{
-        await isMycouple(coupleIdx, scheduleIdx, "schedule");
-
         // const sql = "UPDATE schedule SET is_delete = true WHERE idx = $1 AND couple_idx = $2"
         const sql = "DELETE FROM schedule WHERE idx = $1 AND couple_idx = $2"
         const values = [scheduleIdx, coupleIdx]
